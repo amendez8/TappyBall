5cf07713a12f1b8c94f96e70be05b51a23121fe1
Anthony Mendez <anthonymendez516@gmail.com>
2017-07-03 16:59:41 -0400

    commiting entire game

---
+++ Assets/_Scripts/UiManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class UiManager : MonoBehaviour {
+
+    public GameObject gameOverPanel;
+    public GameObject startUi;
+    public GameObject gameOverText;
+    public static UiManager instance;
+    public Text scoreText;
+    public Text highScoreText;
+
+	// Use this for initialization
+	void Start () {
+        if (instance == null)
+            instance = this;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        scoreText.text = ScoreManager.instance.score.ToString();
+	}
+
+    public void GameStart()
+    {
+        startUi.SetActive(false);
+    }
+
+    public void GameOver()
+    {
+       // gameOverText.SetActive(true);
+        highScoreText.text = "High Score: " + PlayerPrefs.GetInt("HighScore");
+        gameOverPanel.SetActive(true);
+    }
+
+    public void Replay()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void Menu()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
